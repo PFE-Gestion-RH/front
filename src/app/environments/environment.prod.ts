dcf110c344c7446bab234248d30c93af7a05a592
fatma zaibi <fatmaa.zaibi@gmail.com>
2026-04-16 22:32:34 +0200

    Update environment.prod.ts

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://back-production-31f1.up.railway.app',
+    apiUrl: 'https://back-production-31f1.up.railway.app/api',
     hubUrl: 'https://back-production-31f1.up.railway.app/notificationHub'
 };
