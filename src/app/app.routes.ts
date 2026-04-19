015d556e9000f340c4fb20251ddb64a84b520204
fatma zaibi <fatmaa.zaibi@gmail.com>
2026-04-19 22:29:34 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -43,7 +43,7 @@ export const routes: Routes = [
       
 
       // ── Admin ─────────────────────────────────────────────────────────────
-      { path: 'admin', component: AdminDahboard, data: { role: 'Admin' } },
+      { path: 'admin', component: Dashboard, data: { role: 'Admin' } },
       { path: 'admin/users', component: Users, data: { role: 'Admin' } },
       { path: 'admin/teams', component: Teams, data: { role: 'Admin' } },
       { path: 'admin/leave-categories', component: LeaveCategories, data: { role: 'Admin' } },
